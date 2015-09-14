4446be538e84987873e46d534f433570cd453f3e
sco-bo <scottebobbitt@gmail.com>
2015-09-14 12:10:07 -0400

    fix array modifaction issue

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,93 +1,78 @@
-class Player #will choose moves
-  attr_accessor :name, :x_or_o
+module TicTacToe
+  class Player 
+    attr_accessor :symbol
 
-  def initialize(name, x_or_o)
-    @name = name
-    @x_or_o = x_or_o
+    def initialize(symbol)
+      @symbol = symbol
+    end
+
   end
 
-end
+  class Board 
+    attr_reader :spaces
 
-class Board #will hold the state of the moves
-  def initialize
-    @spaces = Array.new(9)
-  end
+    def initialize
+      @spaces = Array.new(9)
+    end
 
-  def to_s
-    output = ""
-    0.upto(8) do |position|
-      output << "#{@spaces[position] || position}"
-      case position % 3 
-      when 0, 1 then output << " | "
-      when 2 then output << "\n-----------\n" unless position == 8
+    def to_s
+      output = ""
+      0.upto(8) do |position|
+        output << "#{@spaces[position] || position}"
+        case position % 3 
+        when 0, 1 then output << " | "
+        when 2 then output << "\n-----------\n" unless position == 8
+        end
       end
+      output
     end
-    output
-  end
 
-  def change_board
-    @array[choice] = player_name
-  end
-
-  def space_available? 
-    if @space[choice].nil?
-      return false
-    else
-      return true
-    end
-  end
-
-end
-
-class Game #will run game and declare winner. What should this object have? 
-  def initialize
-    play_game
-  end
-
-  def play_game
-    player1 = Player.new(player_name(1), 'X')
-    player2 = Player.new(player_name(2), 'O')
-    puts Board.new
-    move
-  end
-
-  def player_name(number)
-    puts "What is your name, player #{number}?"
-    nom = gets.chomp
-    puts "Welcome #{nom}!"
-  end
-
-  def move
-    player1_turn = true
-    while victory != true
-      puts "Where would you like to move?"
-      choice = gets.chomp
-      if choice.space_available? 
-        
+    def space_available(cell, sym) 
+      if spaces[cell].nil?
+        spaces[cell] = sym
       else
-        puts "Sorry, that space is not available."
+        puts "Space unavailable"
       end
-    turn
     end
   end
 
-  def turn
-    if player1_turn
-      player1_turn = false
-    else
-      player1_turn = true
+  class Game < Board
+    attr_reader :player1, :player2
+
+    def initialize
+      super
+      play_game
     end
+
+    def play_game
+      @player1 = Player.new("X")
+      @player2 = Player.new("O")
+      puts Board.new
+      @current_turn = 1
+      turn
+    end
+
+    def move(player)
+      while victory != true
+        puts "Where would you like to move?"
+        choice = gets.chomp.to_i
+        space_available(choice, player.symbol)
+        puts self
+        @current_turn += 1
+        turn
+      end
+    end
+
+    def turn
+      @current_turn.even? ? move(@player2) : move(@player1)
+    end
+
+    def victory
+      #still working on this
+    end
+
   end
-
-
-  def victory
-  end
-
 
 end
 
-class Cell
-end
-
-
-puts Game.new
+puts TicTacToe::Game.new
